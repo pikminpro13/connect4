9b7d0b97168f387da20d359ae706c7b806a9b5ed
Programming <Programming@lab08.flpl.local>
2015-04-04 12:06:12 -0500

    Added win condition checking

--- something.js
+++ something.js
@@ -1,6 +1,8 @@
  $(document).ready(function(){
 	// $("#container").on("click", ".grid-cell", function(){
 		//.
+
+	// 1 means red player and 2 means black player	
 	var color = 1;
 	var history = [];
 	var board = [ [], [], [], [], [], [] ];
@@ -47,16 +49,135 @@
 	});
 
 	function GotWinner(curRow, curColumn){
+
+		// Holds the color of the last piece played
 		var start = board[curRow][curColumn];
 
-		function checkUpDown(start){
-
+		if( checkUpDown(start, curRow, curColumn) ){
+			console.log("You win!");
+		}
+		else if( checkLeftRight(start, curRow, curColumn) ){
+			console.log("YOU win!");
+		}
+		else if(checkUpRightAndDownLeft(start, curRow, curColumn)){
+			console.log("You Win!");
+		}
+		else if(checkUpLeftAndDownRight(start, curRow, curColumn)){
+			console.log("You Win!");
 		}
 
 
-		
+
+	}
+
+	function checkUpDown(start, curRow, curColumn){
+		var numFound = 0;
+
+		for (var i = curRow; i >= 0; i--) {
+			if(start == board[i][curColumn]){
+				numFound++;
+			}
+			else 
+				break;
+		}
+		for (var i = curRow+1; i <= 5; i++) {
+			if(start == board[i][curColumn]){
+				numFound++;
+			}
+			else 
+				break;
+		}
+
+		return (numFound >= 4);
+
+	}
+
+	function checkLeftRight(start, curRow, curColumn){
+			var numFound = 0;
+
+		for (var i = curColumn; i >= 0; i--) {
+			if(start == board[curRow][i]){
+				numFound++;
+			}
+			else 
+				break;
+		}
+		for (var i = curColumn+1; i <= 6; i++) {
+			if(start == board[curRow][i]){
+				numFound++;
+			}
+			else 
+				break;
+		}
+
+		return (numFound >= 4);
+	}
+
+	// Checks for a "forward slash" win diagonal
+	function checkUpRightAndDownLeft(start, curRow, curColumn){
+		var numFound = 0;
+
+		var row = curRow;
+		var col = curColumn;
+		while(row >= 0 && col <= 6){
+			if(start == board[row][col]){
+				numFound++;
+			}
+			else
+				break;
+
+			row--;
+			col++;
+		}
+
+		row = curRow+1;
+		col = curColumn-1;
+		while(row <=5 && col >= 0){
+			if(start == board[row][col]){
+				numFound++;
+			}
+			else
+				break;
+
+			row++;
+			col--;
+		}
+
+		return (numFound >= 4);
 	}
 
 
+	// Checks for a "back slash" win diagonal
+	function checkUpLeftAndDownRight(start, curRow, curColumn){
+		var numFound = 0;
+
+		var row = curRow;
+		var col = curColumn;
+		while(row >= 0 && col >= 0){
+			if(start == board[row][col]){
+				numFound++;
+			}
+			else
+				break;
+
+			row--;
+			col--;
+		}
+
+		row = curRow+1;
+		col = curColumn+1;
+		while(row <=5 && col <= 6){
+			if(start == board[row][col]){
+				numFound++;
+			}
+			else
+				break;
+
+			row++;
+			col++;
+		}
+
+		return (numFound >= 4);		
+	}
 
 });
